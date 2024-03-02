20e7925b3f0cbf300984dcb468e34603bb049587
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-03-02 15:24:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Define the student object
+const student = {
+    name: 'John'
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Test the functionality
+console.log(student.getKeys()); // Output: ['name']
